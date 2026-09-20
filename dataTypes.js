da34f8ca64cd8ba585696699ac2f91e54bd83a50
promitkrishno <124575065+promitkrishno@users.noreply.github.com>
2026-09-21 00:17:21 +0600

    data types

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+let name ="Promit"// String
+let age =24// Number
+let logedIn=true// Boolean
+//null=> state of nothingness
+let myVariable=null//null
+let myVariable2=undefined//undefined state of not defined
+symbol1=Symbol("mySymbol")//symbol unique and immutable
+object1={name:"Promit",age:24}//object
+console.log(typeof "Promit")//string
+console.log(typeof 24)//number
+console.log(typeof true)//boolean
+console.log(typeof null)//object (this is a known quirk in JavaScript)
+console.log(typeof undefined)//undefined
+console.log(typeof symbol1)//symbol
+console.log(typeof object1)//object
