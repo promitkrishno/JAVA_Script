8e9768857f8b1258ef957ae660746c2a2daccff2
promitkrishno <124575065+promitkrishno@users.noreply.github.com>
2026-09-24 23:26:12 +0600

    data type summary

---
+++ dataTypes_summary.js
@@ -0,0 +1,49 @@
+// primitive data types
+// 7 types of primitive data types in JavaScript
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+
+const score = 100; // number
+const scoreValue = 100.5; // number
+const isLoggedIn = false; // boolean
+const outsideTemp = null; // null
+const user = undefined; // undefined
+const user1 = undefined; // undefined
+const id = Symbol("123"); // symbol
+const id1 = Symbol("123"); // symbol // variable id and id1 are not equal because symbol is unique
+const bigNumber = 1234567890123456789012345678901234567890n; // BigInt
+console.log(id === id1); // false
+
+// non-primitive (reference) data types
+// 1. Object
+// 2. Array
+// 3. Function
+
+const heros=["ironman","spiderman","thor"]; // array
+
+let myObj={
+    name:"John",
+    age:30,
+    isLoggedIn:true
+} // object
+
+const myFunction = function(){
+      console.log("Hello World");
+}
+
+console.log( typeof score); // number
+console.log( typeof scoreValue); // number
+console.log( typeof isLoggedIn); // boolean
+console.log( typeof outsideTemp); // object
+console.log( typeof user); // undefined
+console.log( typeof id); // symbol
+console.log( typeof bigNumber); // bigint
+console.log( typeof heros); // object
+console.log( typeof myObj); // object
+console.log( typeof myFunction); // function
